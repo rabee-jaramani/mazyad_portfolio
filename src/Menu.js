14ba1512d257802af253884c1b6c5c6bfe3bb59a
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-06-12 03:50:09 +0400

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -3,23 +3,44 @@ import React from 'react'
 import music from './music/music.mp3'
 
 export default class Menu extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={musin_on:false}
+    }
+    
 
+    play_sound(){
+        const audioEl = document.getElementsByClassName("audio-element")[0]
+        if(this.state.musin_on){
+            audioEl.pause()
+            this.setState({
+                musin_on:false
+            })
+        }
+        else{
+            audioEl.play()
+            this.setState({
+                musin_on:true
+            })
+        }
+       
+        document.querySelector('.volume').classList.toggle('fa-volume-up')
+        document.querySelector('.volume').classList.toggle('fa-volume-mute')
+
+    }
 componentDidMount(){
     gsap.from('.audio',{marginLeft:'-100vw',delay:1})
     gsap.from('.download-cv',{marginLeft:'200vw',delay:1})
-    const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
+    
 }
-
 render(){
     return (
         <div className='menu'>
-            <audio className="audio-element">
+            <audio className="audio-element" autoPlay={true} muted={false}>
                 <source src={music}></source>
             </audio>
-            <div className='audio'>
-                <i className="fas fa-volume-up"></i>
-                <i className="hide-D fas fa-volume-mute"></i>
+            <div className='audio' onClick={()=>this.play_sound()}>
+                <i className="volume fas fa-volume-mute"></i>
             </div>
             <div className='download-cv'>
                 <p>CV</p>
