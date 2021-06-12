ac973a19623f5dec29a2a8451de8a6e03147b32b
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-06-12 06:28:01 +0400

    a

--- src/animate_functions.js
+++ src/animate_functions.js
@@ -44,12 +44,22 @@ const scroll_trigger_time_lines=(trigger,timeline)=>{
     var section1_t = gsap.timeline();
     var section2_t = gsap.timeline();
     var section3_t = gsap.timeline();
+    var section4_t = gsap.timeline();
+    var section5_t = gsap.timeline();
+    var section6_t = gsap.timeline();
+    var section7_t = gsap.timeline();
+    var section8_t = gsap.timeline();
 
     // refernces for all elements that you want to animate
     var header=document.getElementById('header');
     var section1=document.getElementById('section1');
     var section2=document.getElementById('section2');
     var section3=document.getElementById('section3');
+    var section4=document.getElementById('section4');
+    var section5=document.getElementById('section5');
+    var section6=document.getElementById('section6');
+    var section7=document.getElementById('section7');
+    var section8=document.getElementById('section8');
 
     // define a scroll trigger
     gsap.registerPlugin(ScrollTrigger)
@@ -58,22 +68,65 @@ const scroll_trigger_time_lines=(trigger,timeline)=>{
     header_t.to(header,{filter:"brightness(1.2)",duration:1.5,delay:2})
 
     // animate section 1 and its images
-    section1_t.to(section1,{filter:"brightness(1.2)",duration:0.5})
+    section1_t.to(section1,{filter:"brightness(1.2)",duration:0.2})
+    .from('#model1',{marginLeft:'-90vw',duration:0.2})
+    .from('#model2',{marginLeft:'-90vw',duration:0.2})
+    .from('#model3',{marginLeft:'-90vw',duration:0.2})
+    .from('#model4',{marginLeft:'-90vw',duration:0.2})
+
+    // animate section 2
+    section2_t.to(section2,{filter:"brightness(1.2)",duration:0.2})
+    .from('#maternity1',{marginLeft:'-90vw',duration:0.2})
+    .from('#maternity2',{marginLeft:'-90vw',duration:0.2})
+    .from('#maternity3',{marginLeft:'-90vw',duration:0.2})
+    .from('#maternity4',{marginLeft:'-90vw',duration:0.2})
+    
+    // animate section 3
+    section3_t.to(section3,{filter:"brightness(1.2)",duration:.2})
+    .from('#freestyle1',{marginLeft:'-90vw',duration:0.2})
+    .from('#freestyle2',{marginLeft:'-90vw',duration:0.2})
+    .from('#freestyle3',{marginLeft:'-90vw',duration:0.2})
+    .from('#freestyle4',{marginLeft:'-90vw',duration:0.2})
+
+    // animate section 4
+    section4_t.to(section4,{filter:"brightness(1.2)",duration:.2})
     .from('#baby1',{marginLeft:'-90vw',duration:0.2})
     .from('#baby2',{marginLeft:'-90vw',duration:0.2})
     .from('#baby3',{marginLeft:'-90vw',duration:0.2})
     .from('#baby4',{marginLeft:'-90vw',duration:0.2})
-    
-    
-    // animate section 2
-    section2_t.to(section2,{filter:"brightness(1.2)",duration:1.5})
-    // animate section 3
-    section3_t.to(section3,{filter:"brightness(1.2)",duration:1.5})
-
+    // animate section 5
+    section5_t.to(section5,{filter:"brightness(1.2)",duration:.2})
+    .from('#cosmetic1',{marginLeft:'-90vw',duration:0.2})
+    .from('#cosmetic2',{marginLeft:'-90vw',duration:0.2})
+    .from('#cosmetic3',{marginLeft:'-90vw',duration:0.2})
+    .from('#cosmetic4',{marginLeft:'-90vw',duration:0.2})
+    // animate section 6
+    section6_t.to(section6,{filter:"brightness(1.2)",duration:.2})
+    .from('#poster1',{marginLeft:'-90vw',duration:0.2})
+    .from('#poster2',{marginLeft:'-90vw',duration:0.2})
+    .from('#poster3',{marginLeft:'-90vw',duration:0.2})
+    .from('#poster4',{marginLeft:'-90vw',duration:0.2})  
+    // animate section 7
+    section7_t.to(section7,{filter:"brightness(1.2)",duration:.2})
+    .from('#food1',{marginLeft:'-90vw',duration:0.2})
+    .from('#food2',{marginLeft:'-90vw',duration:0.2})
+    .from('#food3',{marginLeft:'-90vw',duration:0.2})
+    .from('#food4',{marginLeft:'-90vw',duration:0.2})  
+    // animate section 8
+    section8_t.to(section8,{filter:"brightness(1.2)",duration:.2})
+    .from('#drink1',{marginLeft:'-90vw',duration:0.2})
+    .from('#drink2',{marginLeft:'-90vw',duration:0.2})
+    .from('#drink3',{marginLeft:'-90vw',duration:0.2})
+    .from('#drink4',{marginLeft:'-90vw',duration:0.2})  
     // CALL SCROLL TRIGGER FUNCTION FOR EACH ELEMENT
     scroll_trigger_time_lines(section1,section1_t);
     scroll_trigger_time_lines(section2,section2_t);
     scroll_trigger_time_lines(section3,section3_t);
+    scroll_trigger_time_lines(section4,section4_t);
+    scroll_trigger_time_lines(section5,section5_t);
+    scroll_trigger_time_lines(section6,section6_t);
+    scroll_trigger_time_lines(section7,section7_t);
+    scroll_trigger_time_lines(section8,section8_t);
 
   }
     
--- src/sections_images_list.js
+++ src/sections_images_list.js
@@ -1,45 +1,137 @@
-import img1 from './images/img1.jpg'
-import img2 from './images/img2.jpg'
-import img3 from './images/img3.jpg'
-import img4 from './images/img4.jpg'
+import model1 from './images/models/model1.png'
+import model2 from './images/models/model2.png'
+import model3 from './images/models/model3.png'
+import model4 from './images/models/model4.png'
+import maternity1 from './images/maternity/maternity1.png'
+import maternity2 from './images/maternity/maternity2.png'
+import maternity3 from './images/maternity/maternity3.png'
+import maternity4 from './images/maternity/maternity4.png'
+import freestyle1 from './images/freestyle/freestyle1.png'
+import freestyle2 from './images/freestyle/freestyle2.png'
+import freestyle3 from './images/freestyle/freestyle3.png'
+import freestyle4 from './images/freestyle/freestyle4.png'
+// 
+import baby1 from './images/babies/baby1.png'
+import baby2 from './images/babies/baby2.png'
+import baby3 from './images/babies/baby3.png'
+import baby4 from './images/babies/baby4.png'
+import cosmetic1 from './images/cosmetics/cosmetic1.png'
+import cosmetic2 from './images/cosmetics/cosmetic2.png'
+import cosmetic3 from './images/cosmetics/cosmetic3.png'
+import cosmetic4 from './images/cosmetics/cosmetic4.png'
+import poster1 from './images/posters/poster1.png'
+import poster2 from './images/posters/poster2.png'
+import poster3 from './images/posters/poster3.png'
+import poster4 from './images/posters/poster4.png'
+import food1 from './images/food/food1.png'
+import food2 from './images/food/food2.png'
+import food3 from './images/food/food3.png'
+import food4 from './images/food/food4.png'
+import drink1 from './images/drinks/drink1.png'
+import drink2 from './images/drinks/drink2.png'
+import drink3 from './images/drinks/drink3.png'
+import drink4 from './images/drinks/drink4.png'
+
+
 
 
 
 export const sections_images_list=[
     {
         section_id:'section1',
-        section_name:'section name 1',
+        section_name:'MODELS',
         fullScreen_div:'fullScreen_div1',
         key:1,
         images:[
-            {image_id:'baby1',image_src:img1},
-            {image_id:'baby2',image_src:img2},
-            {image_id:'baby3',image_src:img3},
-            {image_id:'baby4',image_src:img4}
+            {image_id:'model1',image_src:model1},
+            {image_id:'model2',image_src:model2},
+            {image_id:'model3',image_src:model3},
+            {image_id:'model4',image_src:model4}
         ]
     },
     {
         section_id:'section2',
-        section_name:'section name 2',
+        section_name:'MATERNITY',
         fullScreen_div:'fullScreen_div2',
         key:2,
         images:[
-            {image_id:'fashion1',image_src:img1},
-            {image_id:'fashion2',image_src:img2},
-            {image_id:'fashion3',image_src:img3},
-            {image_id:'fashion4',image_src:img4}
+            {image_id:'maternity1',image_src:maternity1},
+            {image_id:'maternity2',image_src:maternity2},
+            {image_id:'maternity3',image_src:maternity3},
+            {image_id:'maternity4',image_src:maternity4}
         ]
     },
     {
         section_id:'section3',
-        section_name:'section name 3',
+        section_name:'FREESTYLE',
         fullScreen_div:'fullScreen_div3',
         key:3,
         images:[
-            {image_id:'wedding1',image_src:img1},
-            {image_id:'wedding2',image_src:img2},
-            {image_id:'wedding3',image_src:img3},
-            {image_id:'wedding4',image_src:img4}
+            {image_id:'freestyle1',image_src:freestyle1},
+            {image_id:'freestyle2',image_src:freestyle2},
+            {image_id:'freestyle3',image_src:freestyle3},
+            {image_id:'freestyle4',image_src:freestyle4}
         ]
-    }
+    },
+    {
+        section_id:'section4',
+        section_name:'BABIES',
+        fullScreen_div:'fullScreen_div4',
+        key:4,
+        images:[
+            {image_id:'baby1',image_src:baby1},
+            {image_id:'baby2',image_src:baby2},
+            {image_id:'baby3',image_src:baby3},
+            {image_id:'baby4',image_src:baby4}
+        ]
+    },
+    {
+        section_id:'section5',
+        section_name:'COSMETICS',
+        fullScreen_div:'fullScreen_div5',
+        key:5,
+        images:[
+            {image_id:'cosmetic1',image_src:cosmetic1},
+            {image_id:'cosmetic2',image_src:cosmetic2},
+            {image_id:'cosmetic3',image_src:cosmetic3},
+            {image_id:'cosmetic4',image_src:cosmetic4}
+        ]
+    },
+    {
+        section_id:'section6',
+        section_name:'POSTERS',
+        fullScreen_div:'fullScreen_div6',
+        key:6,
+        images:[
+            {image_id:'poster1',image_src:poster1},
+            {image_id:'poster2',image_src:poster2},
+            {image_id:'poster3',image_src:poster3},
+            {image_id:'poster4',image_src:poster4}
+        ]
+    },
+    {
+        section_id:'section7',
+        section_name:'FOOD',
+        fullScreen_div:'fullScreen_div7',
+        key:7,
+        images:[
+            {image_id:'food1',image_src:food1},
+            {image_id:'food2',image_src:food2},
+            {image_id:'food3',image_src:food3},
+            {image_id:'food4',image_src:food4}
+        ]
+    },
+    {
+        section_id:'section8',
+        section_name:'DRINKS',
+        fullScreen_div:'fullScreen_div8',
+        key:8,
+        images:[
+            {image_id:'drink1',image_src:drink1},
+            {image_id:'drink2',image_src:drink2},
+            {image_id:'drink3',image_src:drink3},
+            {image_id:'drink4',image_src:drink4}
+        ]
+    },
+  
 ]
